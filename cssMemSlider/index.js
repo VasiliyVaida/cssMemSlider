bf46b106043ca740a12a1cdd5e85a6419529f8c2
Vasiliy <lbonucci476@gmail.com>
2022-05-15 15:35:26 +0300

    init: start cssMemSlider-task (Sunday, May 15, 2021 15:35:30 PM)

---
+++ cssMemSlider/index.js
@@ -0,0 +1,22 @@
+const memesData = [
+  {
+    id: 1,
+    src: './assets/invasion.jpg',
+    descr: 'Я вам сейчас покажу, откуда готовилось нападение',
+  },
+  {
+    id: 2,
+    src: './assets/sadness.jpg',
+    descr: 'Когда где-то идет война, а Франция еще не подняла белый флаг',
+  },
+  {
+    id: 3,
+    src: './assets/for the better.jpg',
+    descr: 'Это же к лучшему, правда?',
+  },
+  {
+    id: 4,
+    src: './assets/modernSolutions.jpg',
+    descr: 'Современные проблемы требуют современных решений',
+  },
+];
