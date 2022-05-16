0e008c4afb78db08e9cd5d652c93b11b484bc2b9
Vasiliy <lbonucci476@gmail.com>
2022-05-16 12:06:27 +0300

    feat: add basic page layout (Mon, May 16, 2022 12:07:00 PM)

--- cssMemSlider/index.js
+++ cssMemSlider/index.js
@@ -20,3 +20,45 @@ const memesData = [
     descr: 'Современные проблемы требуют современных решений',
   },
 ];
+
+const sliderImgArea = document.querySelector('.slider__img-wrapper');
+const sliderTextArea = document.querySelector('.slider__manage-text');
+const sliderControlsArea = document.querySelector('.slider__manage-controls');
+
+const createSliderImages = (arr) => {
+  arr.forEach((item) => {
+    let img = document.createElement('img');
+    img.src = item.src;
+    img.style.width = '100%';
+    img.style.height = '100%';
+    img.style.flex = '0 0 auto';
+    img.classList.add('slider__img');
+    sliderImgArea.append(img);
+  });
+};
+
+const createSliderDescriptions = (arr) => {
+  arr.forEach((item) => {
+    let descr = document.createElement('p');
+    descr.innerHTML = item.descr;
+    descr.style.width = '100%';
+    descr.style.flex = '0 0 auto';
+    descr.classList.add('slider__manage-descr');
+    sliderTextArea.append(descr);
+  });
+};
+
+const createSliderControls = (arr) => {
+  arr.forEach((item) => {
+    let controlOuter = document.createElement('button');
+    controlOuter.classList.add('slider__control-outer');
+    let controlInner = document.createElement('div');
+    controlInner.classList.add('slider__control-inner');
+    controlOuter.append(controlInner);
+    sliderControlsArea.append(controlOuter);
+  });
+};
+
+createSliderImages(memesData);
+createSliderDescriptions(memesData);
+createSliderControls(memesData);
